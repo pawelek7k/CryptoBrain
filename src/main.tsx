cb212ed70199a0e5e8f676cb82c225de52fb8118
pawelek <k.pawelek@vp.pl>
2024-11-25 17:21:50 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import CoinContextProvider from "./context/CoinContext.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <CoinContextProvider>
-        <App />
-      </CoinContextProvider>
-    </BrowserRouter>
+    <CoinContextProvider>
+      <App />
+    </CoinContextProvider>
   </StrictMode>
 );
