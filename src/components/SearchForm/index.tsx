ec2b07e6dc4fe41d879dfe75e6723abf4f67d341
pawelek <k.pawelek@vp.pl>
2024-11-25 19:42:48 +0100

    Update index.tsx

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -8,13 +8,14 @@ export const SearchForm = () => {
   };
 
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
+    <form onSubmit={(e) => e.preventDefault()} className="flex items-center">
       <input
         type="text"
         onChange={inputHandler}
         required
         value={input}
         placeholder="Search for a coin"
+        className="bg-zinc-950 px-4 py-1 outline-none w-[280px] text-zinc-100 rounded-lg border-2 transition duration-100 border-solid focus:border-yellow-950 border-zinc-900"
       />
     </form>
   );
