8c4eeb58e7d447ff5de9dc7357e4afd519c729d1
pawelek <k.pawelek@vp.pl>
2024-11-25 21:39:56 +0100

    Update index.tsx

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,12 +1,15 @@
 import { GiBrain } from "react-icons/gi";
+import { Link } from "react-router-dom";
 
 export const Logo = () => {
   return (
-    <div className="flex items-center gap-2">
-      <GiBrain className="text-yellow-900 w-10 h-10" />
-      <h1 className="text-xl font-semibold">
-        Crypto<span className="text-yellow-900">Brain</span>
-      </h1>
-    </div>
+    <Link to="/">
+      <div className="flex items-center gap-2">
+        <GiBrain className="text-yellow-900 w-10 h-10" />
+        <h1 className="text-xl font-semibold">
+          Crypto<span className="text-yellow-900">Brain</span>
+        </h1>
+      </div>
+    </Link>
   );
 };
