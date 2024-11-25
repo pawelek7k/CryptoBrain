9b3debd14d27e2afdf9129b0dbe8bcece2179de2
pawelek <k.pawelek@vp.pl>
2024-11-25 20:49:38 +0100

    add fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { Navbar } from "./components/Navbar";
-import { Sidebar } from "./components/Sidebar";
 import { RouterIndex } from "./routes/RouterIndex";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Sidebar />
+
       <RouterIndex />
     </>
   );
--- src/routes/RouterIndex.tsx
+++ src/routes/RouterIndex.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Sidebar } from "../components/Sidebar";
 import Coin from "../pages/Coin";
 import Home from "../pages/Home";
 
 export const RouterIndex = () => {
   return (
     <BrowserRouter>
+      <Sidebar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/coin/:coinId" element={<Coin />} />
