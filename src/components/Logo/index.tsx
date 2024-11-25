1d4158f92bfbefa1b9cfc84659faabbdee71f3e1
pawelek <k.pawelek@vp.pl>
2024-11-25 19:12:11 +0100

    Update index.tsx

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,9 +2,11 @@ import { GiBrain } from "react-icons/gi";
 
 export const Logo = () => {
   return (
-    <div>
-      <GiBrain />
-      <h1>CryptoBrain</h1>
+    <div className="flex items-center gap-2">
+      <GiBrain className="text-yellow-900 w-10 h-10" />
+      <h1 className="text-xl font-semibold">
+        Crypto<span className="text-yellow-900">Brain</span>
+      </h1>
     </div>
   );
 };
