2ca51bed095771c19b1c54787901941200530382
pawelek <k.pawelek@vp.pl>
2024-11-25 19:36:21 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import type { CoinType } from "../../types/coin.d.ts";
 import { CoinContext } from "../context/CoinContext";
@@ -9,21 +9,6 @@ const HomePage = () => {
     currency: { name: string; symbol: string };
   };
   const [displayCoin, setDisplayCoin] = useState<CoinType[]>([]);
-  const [input, setInput] = useState<string>("");
-
-  const inputHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    setInput(value);
-    if (value.trim() === "") setDisplayCoin(allCoin);
-  };
-
-  const searchHandler = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const filteredCoins = allCoin.filter((coin) =>
-      coin.name.toLowerCase().includes(input.toLowerCase())
-    );
-    setDisplayCoin(filteredCoins);
-  };
 
   useEffect(() => {
     setDisplayCoin(allCoin);
@@ -31,15 +16,6 @@ const HomePage = () => {
 
   return (
     <div>
-      <form action="" onSubmit={searchHandler}>
-        <input type="text" onChange={inputHandler} required value={input} />
-        <datalist>
-          {allCoin.map((item, index) => (
-            <option key={index} value={item.name} />
-          ))}
-        </datalist>
-        <button>Search</button>
-      </form>
       {displayCoin.slice(0, 10).map((item, index) => (
         <Link to={`/coin/${item.id}`} key={index}>
           <p>{item.name}</p>
