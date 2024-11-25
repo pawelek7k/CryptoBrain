161cc00ee13725d2b564b4b9c61e8ae0dd9ecfe3
pawelek <k.pawelek@vp.pl>
2024-11-25 17:31:59 +0100

    add fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import CoinContextProvider from "./context/CoinContext.tsx";
+import CoinContextProvider from "./context/CoinContextProvider.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,23 @@
+import { useContext, useEffect, useState } from "react";
+import { CoinContext } from "../context/CoinContext";
+
 const HomePage = () => {
-  return <div></div>;
+  const { allCoin, currency } = useContext(CoinContext);
+  const [displayCoin, setDisplayCoin] = useState([]);
+
+  useEffect(() => {
+    setDisplayCoin(allCoin);
+  }, [allCoin]);
+
+  return (
+    <div>
+      {displayCoin.slice(0, 10).map((item, index) => (
+        <div key={index}>
+          <p>{item.name}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default HomePage;
