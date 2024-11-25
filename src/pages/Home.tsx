a1af70a81aacee0ac51e96e8309d91e37cb1c28a
pawelek <k.pawelek@vp.pl>
2024-11-25 21:49:35 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
 import { SearchResult } from "../components/SearchResult";
+import { Trending } from "../components/Trending";
+import { useSearchContext } from "../context/SearchContext";
 
 const HomePage = () => {
-  return <SearchResult />;
+  const { input } = useSearchContext();
+  return (
+    <>
+      {!input && <Trending />}
+      {input && input.length > 0 && <SearchResult />}
+    </>
+  );
 };
 
 export default HomePage;
