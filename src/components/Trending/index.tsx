712b335648654a18d2e2601366dd5354d8bfc0db
pawelek <k.pawelek@vp.pl>
2024-11-26 18:51:04 +0100

    Update index.tsx

--- src/components/Trending/index.tsx
+++ src/components/Trending/index.tsx
@@ -21,12 +21,12 @@ export const Trending = () => {
     <div>
       <h2 className="heading">Trending</h2>
       {trendingData ? (
-        <div className="mt-4 md:mt-10">
-          <ul className="grid grid-cols-5 gap-4 border-2 border-zinc-900 rounded-md p-4 md:grid-cols-1 md:w-80 md:gap-2">
+        <div className="mt-4 lg:mt-10">
+          <ul className="grid grid-cols-5 gap-4 border-2 border-zinc-900 rounded-md p-4 lg:grid-cols-1 lg:w-80 lg:gap-2">
             {trendingData.coins.slice(0, 10).map((coin, index: number) => (
               <li
                 key={index}
-                className="flex flex-col md:flex-row  items-center text-center map-item"
+                className="flex flex-col lg:flex-row  items-center text-center map-item"
               >
                 <img
                   src={coin.item.thumb}
