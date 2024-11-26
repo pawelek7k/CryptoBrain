959a97028f580a14eae85a6b98080e24f4be67c6
pawelek <k.pawelek@vp.pl>
2024-11-26 18:52:27 +0100

    Update index.tsx

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -9,7 +9,7 @@ export const Layout: React.FC<{ children: React.ReactNode }> = ({
     <div className="flex h-screen">
       <Navbar />
       <Sidebar />
-      <main className="pt-20 px-2 overflow-y-auto w-full overflow-hidden lg:pt-20 lg:px-20">
+      <main className="pt-20 px-2 overflow-y-auto w-full overflow-hidden xl:pt-20 xl:px-20">
         {children}
       </main>
     </div>
